322bc2a47bc295a44ef4fa285b8ff69763329595
kevin-ziteng <kevin.ztwang@gmail.com>
2018-06-07 09:17:37 -0400

    Week5 start

---
+++ week5/lab5/src/app/person.ts
@@ -0,0 +1,22 @@
+export class Person {
+    id: number;
+    firstName: string;
+    lastName: string;
+    dateOfBirth: Date;
+    
+
+    // getAge(): Number{
+    //     let today = new Date();
+    //     let age = today.getFullYear() - this.dateOfBirth.getFullYear();
+
+    //     if(
+    //         this.dateOfBirth.getDate() > today.getDate() &&
+    //         this.dateOfBirth.getMonth() > today.getMonth()
+    //     ){
+    //         age --;
+    //     }
+
+    //     return age;
+    // }
+
+}
